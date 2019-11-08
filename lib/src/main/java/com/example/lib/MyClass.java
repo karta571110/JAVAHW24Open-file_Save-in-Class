13d819aef8f6e4890060c13f4df81ddbd3cfc3e7
karta571110 <44608844+karta571110@users.noreply.github.com>
2019-11-08 13:55:40 +0800

    finish

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -1,7 +1,32 @@
 package com.example.lib;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 public class MyClass {
-    public static void main(String args[]){
-        Studentgrade studentgrade=new Studentgrade();
+    public static void main(String args[]) {
+        Studentgrade studentgrade = new Studentgrade();
+        int i=0;
+        try{
+            File file=new File("couse.txt");
+            FileInputStream readIn=new FileInputStream(file);
+            InputStreamReader read=new InputStreamReader(readIn,"Big5");
+            BufferedReader bufferedReader=new BufferedReader(read);
+            String oneLine=null;
+            while ((oneLine=bufferedReader.readLine())!=null){
+                System.out.println(oneLine);
+                studentgrade.getStudentdata(i,oneLine);
+                i++;
+            }
+            read.close();
+        }
+
+        catch (Exception e){
+            System.out.println("Åª¨ú¥X¿ù");
+        }
+        studentgrade.PrintAllData();
     }
 }
--- lib/src/main/java/com/example/lib/Studentgrade.java
+++ lib/src/main/java/com/example/lib/Studentgrade.java
@@ -1,5 +1,33 @@
 package com.example.lib;
 
 public class Studentgrade {
+    String name[] = new String[6];
+    String Calculus[] = new String[6];
+    String IntroductiontoComputer[] = new String[6];
+    String programming[] = new String[6];
+    String StudentData;
+    String Grade;
+    int peoplenumber;
+
+    public void getStudentdata(int i, String studentData) {
+        StudentData = studentData;
+        int Fd = StudentData.indexOf(",");
+        name[i] = StudentData.substring(0, Fd);
+        Grade = StudentData.substring(Fd + 1);
+        int FgradeD = Grade.indexOf(",");
+        int EgradeD = Grade.lastIndexOf(",");
+        Calculus[i] = Grade.substring(0, FgradeD);
+        IntroductiontoComputer[i] = Grade.substring((FgradeD + 1), EgradeD);
+        programming[i] = Grade.substring(EgradeD + 1);
+        peoplenumber++;
+
+    }
+
+    public void PrintAllData(){
+        for(int i=0;i<peoplenumber;i++){
+            System.out.println("姓名:"+name[i]+"\t微積分成績:"+Calculus[i]+"\t計算機概論成績:"+IntroductiontoComputer[i]+"\t程式設計成績:"+programming[i]);
+        }
+    }
+
 
 }
